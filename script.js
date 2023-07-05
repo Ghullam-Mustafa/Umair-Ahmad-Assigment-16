0261137076fe31202bca69143b75e4bc70d573ba
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-05 16:54:16 +0500

    for loop

---
+++ script.js
@@ -0,0 +1,33 @@
+const users = [
+    {name:"Ghullam Mustafa", city:"Faisalabad", age:19, email:"ghullammustafa@gmail.com", status : "active",  id: "123"},
+    {name:"Ghullam Murtaza", city:"Faisalabad", age:17, email:"ghullammurtaza@gmail.com", status : "inactive",  id: "1234"},
+    {name:"Ghullam Hussain", city:"Faisalabad", age:14, email:"ghullamhussain@gmail.com", status : "active",  id: "12345"},
+    {name:"Ghullam Mustafa", city:"Faisalabad", age:15, email:"ghullammustafa1@gmail.com", status : "active",  id: "123456"},
+    {name:"Ghullam Mujtaba", city:"Faisalabad", age:14, email:"ghullammujtaba@gmail.com", status : "active",  id: "1234567"},
+    {name:"Habib", city:"Faisalabad", age:17, email:"habib@gmail.com", status : "inactive",  id: "12345678"},
+    {name:"Haseeb", city:"Faisalabad", age:16, email:"haseeb@gmail.com", status : "active",  id: "123456789"},
+    {name:"Muneeb", city:"Faisalabad", age:15, email:"muneeb@gmail.com", status : "inactive",  id: "1234567890"}
+]
+// for (let i = 0; i < users.length; i++) {
+
+//     console.log(users[i]);
+// }
+
+for ( let user of users) {
+    // console.log(user.id);`
+        for ( let property in user)
+        console.log(property + "=>"+ user[property]);
+}
+
+// function sayHellow(){return " Hellow Mustafa"
+// }
+
+const sayHellow = name => " Hellow " + name
+
+console.log(sayHellow("Mustafa"));
+// let singleUser = users[0];
+
+// for( let property in singleUser){
+//     // console.log(key);
+//     console.log(singleUser[property]);
+// }
